842c4a848980d64e69135e235b9c1839a93e1726
Alex Palmieri <apalmie@uw.edu>
2021-08-21 13:24:31 -0600

    Adjusted package.json and installed a few new packages for testing and mustach view files

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const mustacheExpress = require('mustache-express');
+
 const routes = require("./routes");
 
 const server = express();
+server.engine('mustache', mustacheExpress());
+server.set('view engine', 'mustache');
+
 server.use(express.json());
+
+server.use(express.static('static'));
 server.use(routes);
 
 module.exports = server;
