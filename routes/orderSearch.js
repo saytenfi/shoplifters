746b1b095bacaac583a2751d0b85a010bf801f62
Alex Palmieri <apalmie@uw.edu>
2021-08-27 10:27:19 -0600

    Revert "search and delete"
    This reverts commit 8f68e6ac19de9689623ab9b8377820e516b4eccb.

--- routes/orderSearch.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const order = require("../daos/order");
-
-router.get("/order:id", order.getOrder);
-router.delete("/delete:id", order.deleteOrder);
-module.exports = router;
